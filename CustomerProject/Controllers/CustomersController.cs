940b8860e31e50fb24768fa8957c1baf3dcf81ae
Björn Green <berra_@hotmail.com>
2017-04-12 11:57:29 +0200

    Done ;)

---
+++ CustomerProject/Controllers/CustomersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CustomerProject.Models;
+using static CustomerProject.Models.DataManager;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerProject.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: /<controller>/
+        [Route("index")]
+        public IActionResult Index()
+        {
+            ViewBag.Customers = GetAllCustomers();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            AddCustomer(customer);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            DeleteCustomer(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            ViewBag.Customer = GetCustomer(id);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(Customer customer, int id)
+        {
+            UpdateCustomer(customer);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ CustomerProject/Models/DataManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerProject.Models
+{
+    public static class DataManager
+    {
+        private static List<Customer> customers = new List<Customer>
+        {
+            new Customer {Id = 1, EmployeeCount = 20, CompanyName = "Academy", Address = "Borgarfjordsgatan 4"},
+            new Customer {Id = 2, EmployeeCount = 645, CompanyName = "Academic Work", Address = "Södra Kontoret 12"},
+            new Customer {Id = 3, EmployeeCount = 4, CompanyName = "Kontorsprylar AB", Address = "Häftapparatsgatan 7, Gemträsk"},
+            new Customer {Id = 4, EmployeeCount = 3001, CompanyName = "SEB", Address = "Kungsträdgårdsgatan 8"},
+        };
+
+        public static Customer[] GetAllCustomers()
+        {
+            return customers.ToArray();
+        }
+
+        public static void AddCustomer(Customer customer)
+        {
+            if (customers.Count() > 0)
+                customer.Id = (customers.Max(c => c.Id) + 1);
+            else
+                customer.Id = 1;
+
+            customers.Add(customer);
+
+        }
+
+        internal static Customer GetCustomer(int id)
+        {
+            return customers.FirstOrDefault(c => c.Id == id);
+        }
+
+        internal static void DeleteCustomer(int id)
+        {
+            Customer customer = customers?.FirstOrDefault(c => c.Id == id);
+            customers.Remove(customer);
+        }
+
+        internal static void UpdateCustomer(Customer customer)
+        {
+            int idx = customers.IndexOf(customers.FirstOrDefault(c => c.Id == customer.Id));
+            customers[idx] = customer;
+        }
+    }
+}
